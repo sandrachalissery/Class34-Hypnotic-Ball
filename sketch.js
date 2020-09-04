bfa0f776d4aaab0c791a677fb6b77e41b395e029
sandrachalissery <65679179+sandrachalissery@users.noreply.github.com>
2020-09-04 15:37:06 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+var hypnoticBall;
+var database;
+var position;
+
+function setup(){
+    createCanvas(500,500);
+    
+    database = firebase.database();
+
+    hypnoticBall = createSprite(250,250,10,10);
+    hypnoticBall.shapeColor = "red";
+
+    var hypnoticBallPosition = database.ref('ball/position');
+    hypnoticBallPosition.on("value", readPosition, errorShowed);
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+       writePosition(0,1);
+    }
+    drawSprites();
+}
+
+function readPosition(data){
+    position = data.val();
+    hypnoticBall.x = position.x;
+    hypnoticBall.y = position.y;
+    
+}
+
+function errorShowed(){
+    console.log("error")
+}
+
+function writePosition(x, y){
+    database.ref('ball/position').set({
+       'x': position.x+x,
+       'y': position.y+y 
+    })
+}
